8da97741427479bfa8d96df0b7d5d58828b16509
victorDominic-beep <victorasiwaju16@gmail.com>
2026-07-31 12:41:32 +0100

    Add CRM agent reply webhook endpoint

--- chat.js
+++ chat.js
@@ -74,13 +74,14 @@ const {
       content: msg.content,
     })),
   };
+        
 
-  try {
+  try { 
     const response = await fetch(webhookUrl, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
         "x-api-key": process.env.CHATBOT_API_KEY,
+        "Content-Type": "application/json",    
       },
       body: JSON.stringify(payload),
     });
